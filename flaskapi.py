24350f5f5425358630e7f5d3e5a6a8cf50e584ae
Jonathan Smithers <jpsmithers1@gmail.com>
2023-01-09 18:44:33 +0000

    updated

---
+++ flaskapi.py
@@ -0,0 +1,23 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+tasks = []
+
+@app.route('/tasks', methods=['POST'])
+def add_task():
+  task = request.get_json()
+  tasks.append(task)
+  return '', 201
+
+@app.route('/tasks/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+  task = tasks[task_id]
+  return task
+
+if __name__ == '__main__':
+  app.run(debug=True)
+  
+# to test the api 
+# add item to list - curl -X POST -H "Content-Type: application/json" -d '{"task": "Buy milk"}' http://localhost:5000/tasks
+# to retrieve list - curl http://localhost:5000/tasks/0
