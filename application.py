6465e41bc4a5a92f4432e63d3a52a308bf40b10a
Jonathan Smithers <jpsmithers1@gmail.com>
2023-01-10 14:22:59 +0000

    Updated notes

--- application.py
+++ application.py
@@ -16,14 +16,19 @@ def get_task(task_id):
   return task
 
 if __name__ == '__main__':
-#setting the host to 0.0.0.0 tells the server to listen on all networks, not just to localhost connections, otherwise the server is only accessible from within the container and not on the host when the container is run
+# If running in Azure as a WebApp use app.run() - and make sure pythjong file is named application.py
   app.run()
-  
+# if running in the docker container locally use app.run(host = "0.0.0.0")
+# This tells the server to listen on all networks, not just to localhost connections, otherwise the server is only accessible from within the container and not on the host when the container is run
+
 # to test the api locally
 # add item to list - curl -X POST -H "Content-Type: application/json" -d '{"task": "Buy milk"}' http://localhost:5000/tasks
 # to retrieve list - curl http://localhost:5000/tasks/0
 
-
 # to test the api via the container - use docker run -p 0.0.0.0:8000:5000 <image name>
 # add item to list - curl -X POST -H "Content-Type: application/json" -d '{"task": "Buy milk"}' http://0.0.0.0:8000/tasks
 # to retrieve list - curl http://0.0.0.0:8000/tasks/0
+
+# to test the api when deployed to Azure WebApp
+# add item to list - curl -X POST -H "Content-Type: application/json" -d '{"task": "Buy milk"}' http://<Azure WebApp Name>/tasks
+# to retrieve list - curl http://<Azure WebApp Name>/tasks/0
