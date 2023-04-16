ecc95a135b4249d94ad251a1c9b95e999fd1af55
Jonathan Smithers <jonathansmithers@pop-os.localdomain>
2023-04-16 09:14:21 +0100

    added bdd tests

---
+++ features/steps/testapplication.py
@@ -0,0 +1,15 @@
+from behave import *
+import json
+from application import app
+
+@given('a buy milk task is added to the task list')
+def step_impl(context):
+    response = app.test_client().post("/tasks", json={"task": "Buy milk"})
+    assert response.status_code == 201
+
+@then('buy milk task should be returned in task list')
+def step_impl(context):
+    response = app.test_client().get('/tasks/0')
+    assert response.status_code == 200
+    data = json.loads(response.get_data())
+    assert data["task"] == "Buy milk"
---
+++ features/testapplication.feature
@@ -0,0 +1,5 @@
+Feature: Test Application
+
+  Scenario: add task and list tasks
+     Given a buy milk task is added to the task list
+      Then buy milk task should be returned in task list
